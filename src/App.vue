7868036ce7d96d6f44c7ab0bad61bd71c4205ea0
Iqro-riv <arumisokolowska@gmail.com>
2022-03-05 15:14:58 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <h1>Hello world</h1>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
