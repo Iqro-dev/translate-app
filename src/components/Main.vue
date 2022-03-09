dad6e85902e07d4477e0c7fec91c345950c59cc1
Iqro-riv <arumisokolowska@gmail.com>
2022-03-09 17:35:12 +0100

    fix(main): remove text-field animation

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,8 +2,27 @@
   <Button text="toggle theme" @click="$emit('toggleTheme')" />
   <v-form>
     <v-container class="d-flex flex-row">
-      <v-text-field label="Input" clearable class="pa-10"></v-text-field>
-      <v-text-field label="Input" clearable class="pa-10"></v-text-field>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <v-text-field
+            label="Input"
+            clearable
+            variant="outlined"
+            class="pa-10"
+            v-model="text"
+            clear-icon="mdi-delete"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-text-field
+            :label="[text ? text : 'Text will appear here...']"
+            clearable
+            variant="contained"
+            class="pa-10"
+            disabled="true"
+          ></v-text-field>
+        </v-col>
+      </v-row>
     </v-container>
   </v-form>
 </template>
@@ -15,6 +34,11 @@ export default {
   name: "Main",
   components: {
     Button,
+  },
+  data() {
+    return {
+      text: "",
+    };
   },
 };
 </script>
