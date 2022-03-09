4258903db78ab968ca773d1fac8471ff925dcfc8
Iqro-riv <arumisokolowska@gmail.com>
2022-03-09 17:07:47 +0100

    fix: make toggle theme full height

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,7 @@
 <template>
   <Button text="toggle theme" @click="$emit('toggleTheme')" />
   <v-form>
-    <v-container class="d-flex">
+    <v-container class="d-flex flex-row">
       <v-text-field label="Input" clearable class="pa-10"></v-text-field>
       <v-text-field label="Input" clearable class="pa-10"></v-text-field>
     </v-container>
